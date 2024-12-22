fd3c12926f249ee1c20adefafad6311131430c77
Spolavore <j.vitor.spolavore@gmail.com>
2024-12-22 18:42:58 -0300

    Update simulated_annealing.py

--- metaheuristic/simulated_annealing.py
+++ metaheuristic/simulated_annealing.py
@@ -19,51 +19,68 @@ def parse_input_file(file_path):  ## Lê o arquivo de entrada e extrai as inform
     
     return instances, bins, balls
 
-def evaluate_solution(solution, lower, upper): #Avaliamos a solução e verificamos o lucro mínimo entre os grupos sem ultrapassar a capacidade da mochila.
-
+def evaluate_solution(solution, instances): #Avaliamos a solução e verificamos o lucro mínimo entre os grupos sem ultrapassar a capacidade da mochila.
     best_value = 0
-    for ball in solution:
-        best_value += (ball*(ball + 1))/ 2
-        if ball < lower or ball > upper:
-            return 0;
-    
-    return best_value;
 
-def get_neighbor(solution): # Gera uma nova solução (vizinha) alterando aleatoriamente a inclusão de um item da solução atual.
+    for index, ball in enumerate(solution):
+        if(ball > instances[index][1] or ball < instances[index][0]):
+            return 0
+        else:
+            best_value += (ball*(ball + 1))/ 2
+
+    return best_value 
+
+def get_neighbor(solution, instances): # Gera uma nova solução (vizinha) alterando aleatoriamente a inclusão de um item da solução atual.()
     from_bin = random.randint(0, len(solution) - 1)
     to_bin = random.randint(0, len(solution) - 1)
 
-    while from_bin == to_bin: 
+    while solution[from_bin] == instances[from_bin][0]:
+        from_bin = random.randint(0, len(solution) - 1)
+
+    while solution[to_bin] == instances[to_bin][1]:
         to_bin = random.randint(0, len(solution) - 1)
+
 
     solution[from_bin] -= 1
     solution[to_bin] += 1
 
     return solution
 
-def simulated_annealing(bins, balls, lower, upper, seed, max_iterations): # Algoritmo de Simulated Annealing para encontrar a melhor solução para o problema da mochila.
+def create_initial_solution(bins, balls, instances):
+    initial_solution = [0] * bins
+    for i in range(bins):
+        initial_solution[i] = instances[i][0]
+        balls -= instances[i][0]
+
+    index = 0
+    while balls > 0:
+        max_add =  instances[index][1] - instances[index][0]
+        if(max_add > balls):
+            initial_solution[index] += balls
+            balls -= balls
+        else:
+            initial_solution[index] += max_add
+            balls -= max_add
+        index += 1
+    return initial_solution
+
+def simulated_annealing(bins, balls, instances, seed, max_iterations): # Algoritmo de Simulated Annealing para encontrar a melhor solução para o problema da mochila.
     random.seed(seed)
-    # Inicializar bins vazios
-    current_solution = [0] * bins
-    # Distribuir bolas aleatoriamente
-    for _ in range(balls):
-        bin_choice = random.randint(0, bins - 1)
-        current_solution[bin_choice] += 1
-
-
+    
+    current_solution = create_initial_solution(bins, balls, instances)
     best_solution = current_solution[:]
-    best_value = evaluate_solution(best_solution, lower, upper)
-    temperature = 100.0
-    min_temperature = 0.1
-    cooling_rate = 0.9
+    best_value = evaluate_solution(best_solution, instances)
+
+    temperature = 1.0
+    min_temperature = 0.00001
+    cooling_rate = 0.95
     
     start_time = time.time()
 
     while temperature > min_temperature or max_iterations > 0:
-        neighbor = get_neighbor(current_solution)
-        current_value = evaluate_solution(current_solution, lower, upper)
-        neighbor_value = evaluate_solution(neighbor, lower, upper)
-
+        neighbor = get_neighbor(current_solution, instances)
+        current_value = evaluate_solution(current_solution, instances)
+        neighbor_value = evaluate_solution(neighbor, instances)
         # Se a solução vizinha for melhor ou passar no teste de probabilidade, adotamos ela
         if (neighbor_value > current_value or 
             random.uniform(0, 1) < math.exp((neighbor_value - current_value) / temperature)):
@@ -98,14 +115,10 @@ def main():
     print(f'Dados da instância ${input_file}')
     print(f'Número de bins {bins}\nNúmero de balls {balls}')
 
-    for lowerBound, upperBound in instances:
-        currentInstance = 0
-        print(f"Resolvendo instância {currentInstance + 1}...")
-        simulated_annealing(bins, balls, lowerBound, upperBound, seed, max_iterations)
-        # print(f"Melhor Solução: {best_solution}")
-        # print(f"Melhor Valor: {best_value}")
-        # print("")
-        currentInstance += 1
+    best_solution, best_value = simulated_annealing(bins, balls, instances, seed, max_iterations)
+    # print(f"Melhor Solução: {best_solution}")
+    print(f"Melhor Valor: {best_value}")
+    # print("")
 
 if __name__ == "__main__":
     main()
